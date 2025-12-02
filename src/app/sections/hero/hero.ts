06a1e892489e29b1b9c757b0ab61b15525c16516
Nkululeko Mthethwa <mthethwan44@gmail.com>
2025-12-02 14:23:13 +0200

    Rename hero image

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -14,25 +14,49 @@ images = [
   'assets/img/c_sharp_logo_2_30px.png',
   'assets/img/javascript_30px.png',
   'assets/img/html_5_30px.png',
-
+  'assets/img/Framework_30px.png',
+  'assets/img/figma_30px.png',
+  'assets/img/cloud_function_30px.png',
+  'assets/img/css3_30px.png',
+  'assets/img/github_30px.png',
+  'assets/img/sass_avatar_30px.png',
+  'assets/img/sql_30px.png',
+  'assets/img/typescript_30px.png',
+  'assets/img/visual_studio_code_2019_30px.png',
 ];
 positions: { top: string; left: string; rotation: string; delay: string; scale: string; duration: string; imgIndex: number }[] = [];
 
 ngOnInit() {
+  // this.images.forEach((img, imgIndex) => {
+  //   for (let i = 0; i < 5; i++) {
+  //     const duration = 20 + Math.random() * 20; // 20s to 40s for variation
+  //     this.positions.push({
+  //       top: `${Math.random() * 90}vh`,
+  //       left: `${Math.random() * 95}vw`,
+  //       rotation: `${Math.random() * 360}deg`,
+  //       delay: `${Math.random() * 20}s`,
+  //       scale: `${0.5 + Math.random() * 0.8}`,
+  //       duration: `${duration}s`,
+  //       imgIndex
+  //     });
+  //   }
+  // });
+
   this.images.forEach((img, imgIndex) => {
-    for (let i = 0; i < 5; i++) {
-      const duration = 20 + Math.random() * 20; // 20s to 40s for variation
-      this.positions.push({
-        top: `${Math.random() * 90}vh`,
-        left: `${Math.random() * 95}vw`,
-        rotation: `${Math.random() * 360}deg`,
-        delay: `${Math.random() * 20}s`,
-        scale: `${0.5 + Math.random() * 0.8}`,
-        duration: `${duration}s`,
-        imgIndex
-      });
-    }
+  const duration = 20 + Math.random() * 20; // 20s to 40s
+  this.positions.push({
+    top: `${Math.random() * 90}vh`,
+    left: `${Math.random() * 95}vw`,
+    rotation: `${Math.random() * 360}deg`,
+    delay: `${Math.random() * 20}s`,
+    scale: `${0.5 + Math.random() * 0.8}`,
+    duration: `${duration}s`,
+    imgIndex
   });
+});
+
+
+
 }
 
 }
