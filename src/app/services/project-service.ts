bfc9a4ebf7a16836130f451d854eeec76e2280b7
Nkululeko Mthethwa <mthethwan44@gmail.com>
2025-12-08 19:11:44 +0200

    updated projects featured images

--- src/app/services/project-service.ts
+++ src/app/services/project-service.ts
@@ -26,8 +26,28 @@ export class ProjectService {
   theSolution: "EstateFlow centralizes the entire real estate workflow into one unified platform. Sellers can easily submit and manage property listings, buyers can browse available properties and place offers, and admins can approve listings, monitor activity, and oversee platform analytics. With real-time offer tracking, structured deal management, and automated notifications, EstateFlow modernizes the real estate transaction lifecycle for speed, clarity, and efficiency.",
   theBenefits: "Buyers gain greater transparency and faster access to property details and offer outcomes. Sellers get streamlined listing management and better control over incoming offers. Admins benefit from a centralized dashboard that reduces manual oversight and improves operational clarity. Overall, EstateFlow enhances accessibility, accountability, and decision-making across the entire real estate ecosystem."
 },
- {
+{
   id: 2,
+  title: 'Healthcare Appointment Booking System – Angular, ASP.NET Core, SQL Server',
+  category: "Healthcare Management",
+  shortDesc: 'A full-stack web application for booking and managing healthcare appointments, built with ASP.NET Core Web API, Angular, and SQL Server.',
+  imgUrl: 'assets/img/HealthCare_Appointment_System – Angular  ASP.NET Core  SQL Server.png',
+  desc: 'This healthcare platform allows patients to book appointments, doctors to manage schedules, and administrators to oversee users and reporting. Built with ASP.NET Core Web API, Angular, and SQL Server, it applies SOLID principles, JWT authentication, and role-based access control (RBAC) to ensure scalability, maintainability, and secure role-specific actions. The system improves transparency, reduces scheduling conflicts, and streamlines healthcare communication in a centralized, digital platform.',
+  liveDemo: "https://happy-ground-0a36d730f.3.azurestaticapps.net/",
+  images: {
+          img1: "assets/img/hcs-1.png",
+          img2: "assets/img/hcs-2.png",
+          img3: "assets/img/hcs-3.png",
+          img4: "assets/img/hcs-4.png",
+      },
+  gitHubLink: "https://github.com/nsmthethwa44/HCBS-Angular-ASP.NET-Core-SQL-Server",
+  vidUrl: "https://youtu.be/GueNLZ4QtII",
+  theProblem: "Healthcare appointment booking often relies on outdated or manual methods, leading to double bookings, poor communication, and wasted time for both patients and providers.",
+  theSolution: "The Healthcare Appointment Booking System digitizes scheduling by allowing patients to book online, doctors to manage availability, and administrators to oversee the platform with secure, role-based access.",
+  theBenefits: "Reduces scheduling conflicts, improves communication, and provides a secure, scalable, and transparent platform for managing appointments across patients, doctors, and administrators."
+},
+ {
+  id: 3,
   title: 'Car Auction System – Angular, ASP.NET Core, SQL Server',
    category: "Car Management",
   shortDesc: 'A full-stack car auction platform that enables car owners to list vehicles and bidders to place and track real-time bids using Angular, ASP.NET Core, and SQL Server.',
@@ -47,26 +67,7 @@ export class ProjectService {
   theBenefits: 'Buyers get transparent, real-time updates on bidding status and vehicle details. Sellers gain wider reach and better control over their listings. Administrators benefit from structured oversight, improved security, and reduced manual workload. The system modernizes the auction experience with improved accessibility, accountability, and data-driven decision-making.'
 },
 
-{
-  id: 3,
-  title: 'Healthcare Appointment Booking System – Angular, ASP.NET Core, SQL Server',
-  category: "Healthcare Management",
-  shortDesc: 'A full-stack web application for booking and managing healthcare appointments, built with ASP.NET Core Web API, Angular, and SQL Server.',
-  imgUrl: 'assets/img/HealthCare_Appointment_System – Angular  ASP.NET Core  SQL Server.png',
-  desc: 'This healthcare platform allows patients to book appointments, doctors to manage schedules, and administrators to oversee users and reporting. Built with ASP.NET Core Web API, Angular, and SQL Server, it applies SOLID principles, JWT authentication, and role-based access control (RBAC) to ensure scalability, maintainability, and secure role-specific actions. The system improves transparency, reduces scheduling conflicts, and streamlines healthcare communication in a centralized, digital platform.',
-  liveDemo: "https://happy-ground-0a36d730f.3.azurestaticapps.net/",
-  images: {
-          img1: "assets/img/hcs-1.png",
-          img2: "assets/img/hcs-2.png",
-          img3: "assets/img/hcs-3.png",
-          img4: "assets/img/hcs-4.png",
-      },
-  gitHubLink: "https://github.com/nsmthethwa44/HCBS-Angular-ASP.NET-Core-SQL-Server",
-  vidUrl: "https://youtu.be/GueNLZ4QtII",
-  theProblem: "Healthcare appointment booking often relies on outdated or manual methods, leading to double bookings, poor communication, and wasted time for both patients and providers.",
-  theSolution: "The Healthcare Appointment Booking System digitizes scheduling by allowing patients to book online, doctors to manage availability, and administrators to oversee the platform with secure, role-based access.",
-  theBenefits: "Reduces scheduling conflicts, improves communication, and provides a secure, scalable, and transparent platform for managing appointments across patients, doctors, and administrators."
-}
+
 
   ]
 
